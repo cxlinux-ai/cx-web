b7d7ad1c26bdbd2ae975a9e4da87e539028d846c
0xBigotry7 <garyxuejingzhou@gmail.com>
2026-02-22 08:23:16 +0700

    chore: remove compare component (cleanup)

--- client/src/components/compare/index.ts
+++
@@ -1,8 +0,0 @@
-export { CompareHero } from './CompareHero';
-export { FeatureTable } from './FeatureTable';
-export { UseCases } from './UseCases';
-export { WhenToUse } from './WhenToUse';
-export { MigrationGuide } from './MigrationGuide';
-export { CompareFAQ } from './CompareFAQ';
-export { CompareCTA } from './CompareCTA';
-export { InternalLinks } from './InternalLinks';
